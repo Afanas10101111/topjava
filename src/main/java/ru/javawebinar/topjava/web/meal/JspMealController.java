d70b4698c0ddfd81b4ce662c437a7454fbd0d46b
AF <af@af.com>
2021-11-14 22:55:34 +0300

    [HW06] Complete the standard task

---
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -0,0 +1,87 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.service.MealService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
+
+@Controller
+public class JspMealController extends MealController {
+    public static final String MEAL_ATTR = "meal";
+    public static final String MEALS_ATTR = "meals";
+
+    public static final String MEALS_LINK = "/meals";
+    public static final String REDIRECT_MEALS = "redirect:/meals";
+    public static final String MEAL_FORM_LINK = "/mealForm";
+
+    public JspMealController(MealService service) {
+        super(service);
+    }
+
+    @GetMapping(MEALS_LINK)
+    public String getAllForUi(Model model) {
+        model.addAttribute(MEALS_ATTR, getAll());
+        return MEALS_LINK;
+    }
+
+    @GetMapping(MEALS_LINK + "/delete")
+    public String deleteForUi(HttpServletRequest request) {
+        delete(getId(request));
+        return REDIRECT_MEALS;
+    }
+
+    @GetMapping(MEALS_LINK + "/update")
+    public String updateForUi(HttpServletRequest request) {
+        request.setAttribute(MEAL_ATTR, get(getId(request)));
+        return MEAL_FORM_LINK;
+    }
+
+    @GetMapping(MEALS_LINK + "/create")
+    public String createForUi(Model model) {
+        model.addAttribute(MEAL_ATTR, new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000));
+        return MEAL_FORM_LINK;
+    }
+
+    @GetMapping(MEALS_LINK + "/filter")
+    public String filterForUi(HttpServletRequest request) {
+        LocalDate startDate = parseLocalDate(request.getParameter("startDate"));
+        LocalDate endDate = parseLocalDate(request.getParameter("endDate"));
+        LocalTime startTime = parseLocalTime(request.getParameter("startTime"));
+        LocalTime endTime = parseLocalTime(request.getParameter("endTime"));
+        request.setAttribute(MEALS_ATTR, getBetween(startDate, startTime, endDate, endTime));
+        return MEALS_LINK;
+    }
+
+    @PostMapping(MEALS_LINK)
+    public String setForUi(HttpServletRequest request) {
+        Meal meal = new Meal(
+                LocalDateTime.parse(request.getParameter("dateTime")),
+                request.getParameter("description"),
+                Integer.parseInt(request.getParameter("calories"))
+        );
+        if (StringUtils.hasLength(request.getParameter("id"))) {
+            update(meal, getId(request));
+        } else {
+            create(meal);
+        }
+        return REDIRECT_MEALS;
+    }
+
+    private int getId(HttpServletRequest request) {
+        String paramId = Objects.requireNonNull(request.getParameter("id"));
+        return Integer.parseInt(paramId);
+    }
+}
